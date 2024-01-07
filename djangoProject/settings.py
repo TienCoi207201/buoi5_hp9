14975f03ad7188e61cbb8ec2452159261fa1d324
TienCoi207201 <namtien9182@gmail.com>
2024-01-07 12:00:35 +0700

    deploy app

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-qnu=$k2@dw5#a6gnjz1852e%^b9v$*(!iqnpv-xnf^(u_$--ex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = django_heroku.os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = (django_heroku.os.path.join(BASE_DIR, 'static'))
+django_heroku.settings(locals())
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
